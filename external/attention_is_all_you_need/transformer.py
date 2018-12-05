29ce6629b371e8115d8cfe5e763de68c9a576c82
Salman <salkhan23@gmail.com>
2018-12-05 10:46:14 -0500

    Somemore code cleanup

--- external/attention_is_all_you_need/transformer.py
+++ external/attention_is_all_you_need/transformer.py
@@ -348,9 +348,12 @@ class Transformer:
 
 	def make_src_seq_matrix(self, input_seq):
 		src_seq = np.zeros((1, len(input_seq)+3), dtype='int32')
-		src_seq[0,0] = self.i_tokens.startid()
-		for i, z in enumerate(input_seq): src_seq[0,1+i] = self.i_tokens.id(z)
-		src_seq[0,len(input_seq)+1] = self.i_tokens.endid()
+		src_seq[0, 0] = self.i_tokens.startid()
+		for i, z in enumerate(input_seq):
+			src_seq[0, 1+i] = self.i_tokens.id(z)
+
+		src_seq[0, len(input_seq)+1] = self.i_tokens.endid()
+
 		return src_seq
 
 	def decode_sequence(self, input_seq, delimiter=''):
@@ -375,8 +378,11 @@ class Transformer:
 
 		src_pos = Lambda(self.get_pos_seq)(src_seq)
 		tgt_pos = Lambda(self.get_pos_seq)(tgt_seq)
-		if not self.src_loc_info: src_pos = None
+		if not self.src_loc_info:
+			src_pos = None
+
 		enc_output = self.encoder(src_seq, src_pos)
+
 		self.encode_model = Model(src_seq_input, enc_output)
 
 		enc_ret_input = Input(shape=(None, self.d_model))
@@ -388,20 +394,24 @@ class Transformer:
 		self.decode_model.compile('adam', 'mse')
 
 	def decode_sequence_fast(self, input_seq, delimiter=''):
-		if self.decode_model is None: self.make_fast_decode_model()
+		if self.decode_model is None:
+			self.make_fast_decode_model()
+
 		src_seq = self.make_src_seq_matrix(input_seq)
 		enc_ret = self.encode_model.predict_on_batch(src_seq)
 
 		decoded_tokens = []
 		target_seq = np.zeros((1, self.len_limit), dtype='int32')
-		target_seq[0,0] = self.o_tokens.startid()
+		target_seq[0, 0] = self.o_tokens.startid()
 		for i in range(self.len_limit-1):
-			output = self.decode_model.predict_on_batch([src_seq,enc_ret,target_seq])
-			sampled_index = np.argmax(output[0,i,:])
+			output = self.decode_model.predict_on_batch([src_seq, enc_ret, target_seq])
+			sampled_index = np.argmax(output[0, i, :])
 			sampled_token = self.o_tokens.token(sampled_index)
 			decoded_tokens.append(sampled_token)
-			if sampled_index == self.o_tokens.endid(): break
-			target_seq[0,i+1] = sampled_index
+			if sampled_index == self.o_tokens.endid():
+				break
+			target_seq[0, i+1] = sampled_index
+
 		return delimiter.join(decoded_tokens[:-1])
 
 	def beam_search(self, input_seq, topk=5, delimiter=''):
